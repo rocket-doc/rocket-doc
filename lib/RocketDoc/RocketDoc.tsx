be6adf97e3ca44ea17bebf9d9cbe35fe6dfecc00
Nathan Perriolat <nathan.perriolat@fifteen.eu>
2024-11-28 09:59:37 +0100

    1.0.5

--- lib/RocketDoc/RocketDoc.tsx
+++ lib/RocketDoc/RocketDoc.tsx
@@ -1,27 +1,26 @@
 import rocketLogo from "@/assets/rocket-doc.png";
-import { Error, ErrorBoundary } from "@/components/Error/Error";
+import { ErrorBoundary, Error as ErrorElement } from "@/components/Error/Error";
 import { Home } from "@/components/Home/Home";
 import Navbar from "@/components/Navbar/Navbar";
 import { Operation } from "@/components/Operation/Operation";
 import { SchemaRoute } from "@/components/Schema/Schema";
 import SearchModal from "@/components/SearchModal/SearchModal";
-import { AuthInformations, getSavedCredential, SavedCredentials, updateAuthValueFromSchemeValue } from "@/components/SecurityRequirement/schemes";
 import SecurityRequirement from "@/components/SecurityRequirement/SecurityRequirement";
 import '@/index.css';
 import { AppConfig, DefaultConfig, Extensions, UserConfig } from "@/lib/config";
 import { ConfigContext, ExtensionsContext, ModalContext, SpecContext, UserConfigContext } from "@/lib/context";
 import usePersistentState from "@/lib/hooks/persistant";
-import { specificationCredentialsDefaultSchemeName, specificationCredentialsKey } from "@/lib/local_storage";
+import { specificationCredentialsDefaultSchemeName } from "@/lib/local_storage";
 import { ConfigProvider as AntdConfigProvider, Button, Spin, Splitter, theme } from "antd";
 import { OpenAPIObject, SecuritySchemeObject } from "openapi3-ts/oas31";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   BrowserRouter,
   HashRouter,
   Route,
   Routes,
 } from "react-router-dom";
-import { parse as parseYaml } from 'yaml';
+import { useSpecUrlWithSecurity } from "./spec";
 
 export type AppProps = {
   config?: AppConfig;
@@ -73,10 +72,10 @@ export default function RocketDoc({ logo, config, extensions, showFileImport, sp
                             <div className="flex justify-center items-center flex-col min-h-screen">
                               <h1 className="text-2xl mb-2 text-center"> Credentials are required to access OpenAPI specification</h1>
                               <SecurityRequirement
-                                requirement={{ specificationCredentialsDefaultSchemeName: specRequiredSecurityScopes ?? [] }}
+                                requirement={{ [specificationCredentialsDefaultSchemeName]: specRequiredSecurityScopes ?? [] }}
                                 savedCreds={savedCreds}
                                 setSavedCreds={setSavedCreds}
-                                schemes={{ specificationCredentialsDefaultSchemeName: specRequiredSecurity }}
+                                schemes={{ [specificationCredentialsDefaultSchemeName]: specRequiredSecurity }}
                                 typeAsName
                               />
                               <div className="my-2 flex justify-center items-center flex-col">
@@ -87,7 +86,7 @@ export default function RocketDoc({ logo, config, extensions, showFileImport, sp
                               </div>
                             </div> :
                             <Routes>
-                              <Route path="*" element={<Error title="Not Found" message="The page you are looking for does not exist, or not for this spec" />} />
+                              <Route path="*" element={<ErrorElement title="Not Found" message="The page you are looking for does not exist, or not for this spec" />} />
                               <Route index element={<Home />} ErrorBoundary={ErrorBoundary} />
                               <Route
                                 path="/operations/:method/:path"
@@ -123,75 +122,3 @@ function useBodyDarkClass(cfg: UserConfig) {
     }
   }, [cfg.darkMode]);
 }
-
-function useSpecUrlWithSecurity(
-  specUrl: string | undefined,
-  setSpec: (v: OpenAPIObject | null) => void,
-  security: SecuritySchemeObject | undefined,
-) {
-  const [specError, setError] = useState("")
-  const [savedCreds, setSavedCreds] = usePersistentState<SavedCredentials>(specificationCredentialsKey, {});
-  const [loadingSpec, setLoadingSpec] = useState(false);
-
-  const authInfos = useMemo<AuthInformations>(() => {
-    if (!security) {
-      return {}
-    }
-    return updateAuthValueFromSchemeValue(
-      specificationCredentialsDefaultSchemeName,
-      security,
-      getSavedCredential(specificationCredentialsDefaultSchemeName, security, savedCreds),
-      {}
-    )
-  }, [savedCreds])
-
-  // Callback to fetch documentation document
-  const reloadSpec = useCallback(() => {
-    if (!specUrl) return;
-    let url = new URL(specUrl)
-    Object.entries(authInfos.query ?? {}).forEach(([key, value]) => url.searchParams.set(key, value))
-    let headers = new Headers()
-    Object.entries(authInfos.headers ?? {}).forEach(([key, value]) => headers.set(key, value))
-    setLoadingSpec(true)
-    fetch(url.toString(), {
-      headers: headers,
-      method: "GET",
-    }).then((res) => {
-      if (res.status !== 200) {
-        console.warn("could not load spec")
-        setError("Got status " + res.status);
-        return
-      }
-      switch (res.headers.get("content-type")) {
-        case "application/x-yaml":
-        case "text/yaml":
-        case "application/yaml":
-        case "application/yml":
-          res.text().then(content => {
-            return parseYaml(content);
-          })
-          break;
-        default:
-          // Default is JSON
-          return res.json()
-      }
-    }).then(o => {
-      setSpec(o)
-      setError("")
-    }).catch(
-      (e) => {
-        console.warn(`Could not load spec ${e}`)
-        setError(`Could not load spec: ${e}`)
-      }
-    ).finally(() => setLoadingSpec(false))
-  }, [specUrl, authInfos])
-
-  // On mount, fetch the spec if no credentials are required
-  // or if credentials are required and are saved
-  useEffect(() => {
-    if (!specUrl || (security && !getSavedCredential(specificationCredentialsDefaultSchemeName, security, savedCreds))) return
-    reloadSpec()
-  }, [])
-
-  return { loadingSpec, specError, savedCreds, setSavedCreds, authInfos, reloadSpec }
-} 
---
+++ lib/RocketDoc/spec.ts
@@ -0,0 +1,73 @@
+import { AuthInformations, getSavedCredential, SavedCredentials, updateAuthValueFromSchemeValue } from "@/components/SecurityRequirement/schemes";
+import usePersistentState from "@/lib/hooks/persistant";
+import { specificationCredentialsDefaultSchemeName, specificationCredentialsKey } from "@/lib/local_storage";
+import { OpenAPIObject, SecuritySchemeObject } from "openapi3-ts/oas31";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { parse as parseYaml } from 'yaml';
+
+export function useSpecUrlWithSecurity(
+  specUrl: string | undefined,
+  setSpec: (v: OpenAPIObject | null) => void,
+  security: SecuritySchemeObject | undefined,
+) {
+  const [specError, setError] = useState("")
+  const [savedCreds, setSavedCreds] = usePersistentState<SavedCredentials>(specificationCredentialsKey, {});
+  const [loadingSpec, setLoadingSpec] = useState(false);
+
+  const authInfos = useMemo<AuthInformations>(() => {
+    if (!security) {
+      return {}
+    }
+    return updateAuthValueFromSchemeValue(
+      specificationCredentialsDefaultSchemeName,
+      security,
+      getSavedCredential(specificationCredentialsDefaultSchemeName, security, savedCreds),
+      {}
+    )
+  }, [savedCreds])
+
+  // Callback to fetch documentation document
+  const reloadSpec = useCallback(() => {
+    if (!specUrl) return;
+    let url = new URL(specUrl)
+    Object.entries(authInfos.query ?? {}).forEach(([key, value]) => url.searchParams.set(key, value))
+    let headers = new Headers()
+    Object.entries(authInfos.headers ?? {}).forEach(([key, value]) => headers.set(key, value))
+    setLoadingSpec(true)
+    fetch(url.toString(), {
+      headers: headers,
+      method: "GET",
+    }).then((res) => {
+      if (res.status !== 200) {
+        throw `Unexpected status ${res.status}`;
+      }
+      switch (res.headers.get("content-type")) {
+        case "application/x-yaml":
+        case "text/yaml":
+        case "application/yaml":
+        case "application/yml":
+          res.text().then(content => {
+            return parseYaml(content);
+          })
+          break;
+        default:
+          // Default is JSON
+          return res.json()
+      }
+    }).then(o => {
+      setSpec(o)
+      setError("")
+    }).catch((e) => {
+      setError(`Could not load spec: ${e}`)
+    }).finally(() => setLoadingSpec(false))
+  }, [specUrl, authInfos, setError, setSpec])
+
+  // On mount, fetch the spec if no credentials are required
+  // or if credentials are required and are saved
+  useEffect(() => {
+    if (!specUrl || (security && !getSavedCredential(specificationCredentialsDefaultSchemeName, security, savedCreds))) return
+    reloadSpec()
+  }, [])
+
+  return { loadingSpec, specError, savedCreds, setSavedCreds, authInfos, reloadSpec }
+} 
